ae7cf17df2e7e4806ba377bbda39cdc0b83485e9
mirkete <saenzvalietemirko@gmail.com>
2023-10-22 14:59:06 -0300

    design improved - external messages styled

--- web/src/script.js
+++ web/src/script.js
@@ -2,10 +2,10 @@ import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js"
 
 const socket = io()
 const url = new URL(window.location.href)
+const username = localStorage.getItem("username")
 
 function logUser () {
   return new Promise((resolve, reject) => {
-    const username = localStorage.getItem("username")
     if (username) {
       socket.emit("logged-in", username)
       resolve()
@@ -22,14 +22,16 @@ logUser()
     const messagesContainer = document.querySelector(".messages-container")
 
     socket.on("chat-message", (msg) => {
-      messagesContainer.insertAdjacentHTML("beforeend", `<li>${msg}</li>`)
+      console.log({here:msg.username, out:username})
+      const messageClass = msg.username === username ? "own-message" : "external-message"
+      messagesContainer.insertAdjacentHTML("beforeend", `<li class="single-message ${messageClass}">${msg.username}:${msg.message}</li>`)
       messagesContainer.scrollTop = messagesContainer.scrollHeight
     })
 
     form.addEventListener("submit", (e) => {
       e.preventDefault()
       if (message.value) {
-        socket.emit("chat-message", message.value)
+        socket.emit("chat-message", {message: message.value, username })
         message.value = ""
       }
     })
